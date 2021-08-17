9e7ec6b68771efae2a5fec126e2064e3ac403bdf
kouyt5 <1147893200@qq.com>
2021-08-17 16:59:25 +0800

    优化squid启动entrypoint逻辑

--- lonely-app/squid/entrypoint.sh
+++ lonely-app/squid/entrypoint.sh
@@ -6,4 +6,6 @@ chown -R proxy:proxy /var/log/squid/
 echo "创建缓存目录"
 squid -z
 echo "开始squid服务..."
-squid -N
+# entrypoint会续接cmd命令，例如./entrypoint.sh squid -NX, squid -NX为cmd命令。
+# 用下面的代码表示将entrypoint后面的参数传入进来执行。
+exec "$@"

0637702eb4423ddaa8b59319f36e20af5019a86e
kouyt5 <1147893200@qq.com>
2023-06-28 15:28:58 +0800

    添加请求url & method log

--- nginx/src/fuploads/src/main/kotlin/com/chenc/fuploads/component/RequestUrlInterceptor.kt
+++ nginx/src/fuploads/src/main/kotlin/com/chenc/fuploads/component/RequestUrlInterceptor.kt
@@ -1,24 +1,39 @@
 package com.chenc.fuploads.component
 
-import org.springframework.web.servlet.HandlerInterceptor
-import org.springframework.stereotype.Component
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class RequestUrlInterceptor: HandlerInterceptor {
+class RequestUrlInterceptor : HandlerInterceptor {
 
     val log: Logger = LoggerFactory.getLogger(FTPFactory::class.java)
 
-    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+    override fun preHandle(
+            request: HttpServletRequest,
+            response: HttpServletResponse,
+            handler: Any
+    ): Boolean {
+        
+        var ip: String? = getRealIP(request)
+        log.info("curr ip: ${ip}. req_url: ${request.requestURL}. method: ${request.method}")
+        return true
+    }
+
+    private fun ipValid(ip: String?): Boolean {
+        return ip != null && ip.length != 0 && "unknown" != ip
+    }
+
+    private fun getRealIP(request: HttpServletRequest): String? {
         var ip: String? = request.getHeader("x-forwarded-for")
 
         if (ipValid(ip)) {
             // 多次反向代理后会有多个ip值，第一个ip才是真实ip
-            if( ip?.indexOf(",") != -1 ){
-                ip = ip!!.split(",")[0];
+            if (ip?.indexOf(",") != -1) {
+                ip = ip!!.split(",")[0]
             }
         }
         if (!ipValid(ip)) {
@@ -39,11 +54,6 @@ class RequestUrlInterceptor: HandlerInterceptor {
         if (!ipValid(ip)) {
             ip = request.getRemoteAddr()
         }
-        log.info("curr ip: ${ip}")
-        return true;
-     }
-
-     private fun ipValid(ip: String?) : Boolean {
-        return ip != null && ip.length != 0 && "unknown" != ip
-     }
+        return ip
+    }
 }

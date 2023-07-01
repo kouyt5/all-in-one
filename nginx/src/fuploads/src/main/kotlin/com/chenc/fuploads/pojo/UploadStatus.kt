84bdb70b2ab7ae90e3e990fc7d099e398f436670
kouyt5 <1147893200@qq.com>
2023-07-01 18:37:26 +0800

    文件自动解压实现

--- nginx/src/fuploads/src/main/kotlin/com/chenc/fuploads/pojo/UploadStatus.kt
+++ nginx/src/fuploads/src/main/kotlin/com/chenc/fuploads/pojo/UploadStatus.kt
@@ -4,5 +4,8 @@ enum class UploadStatus(val code: Int, val message: String) {
     SUCCESS(200, "OK"),
     UNKNOWN_ERROR(500, "未知错误"),
     DIRECTORY_NOT_EXIST(400, "目录不存在"),
-    MULTIFILE_ERROR(401, "多文件上传错误")
+    MULTIFILE_ERROR(401, "多文件上传错误"),
+    
+    EXTRACT_ERROR(501, "解压未知错误"),
+    EXTRACT_NO_NEED(201, "非压缩文件"),
 }
